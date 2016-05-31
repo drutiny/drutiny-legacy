1498903fabfab0df35683c7d5f9b52916333438c
Sean Hamlin <sean.hamlin@acquia.com>
2016-05-31 12:37:48 +1200

    New check for the XML sitemap base URL.

---
+++ src/Drush/XMLSiteMapBaseUrl.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SiteAudit\Drush;
+
+use SiteAudit\Base\Check;
+use SiteAudit\AuditResponse\AuditResponse;
+use SiteAudit\Executor\DoesNotApplyException;
+
+class XMLSiteMapBaseUrl extends Check {
+  public function check() {
+    $response = new AuditResponse('variable/xmlsitemap_base_url', $this);
+
+    $response->test(function ($check) {
+      $context = $check->context;
+
+      // If the module is disabled, then no xmlsitemap.
+      if ($check->context->drush->moduleEnabled('xmlsitemap')) {
+
+        // This defaults to $GLOBALS['base_url'] which is bad.
+        $base_url = (string) $context->drush->getVariable('xmlsitemap_base_url', '');
+        if (empty($base_url)) {
+          $check->setToken('base_url', '[empty]');
+          return FALSE;
+        }
+
+        $check->setToken('base_url', $base_url);
+        if (!preg_match('#^https://www\.[-a-z]{1,63}\.gov\.au$#', $base_url)) {
+          return FALSE;
+        }
+      }
+      // If the module is not enabled, then this check does not apply.
+      else {
+        throw new DoesNotApplyException();
+      }
+
+      return TRUE;
+    });
+
+    return $response;
+  }
+}
