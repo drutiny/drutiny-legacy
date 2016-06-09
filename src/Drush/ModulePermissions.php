77d2e02120c3f9aab8af568f7393ea4b903eb31f
Sean Hamlin <sean.hamlin@acquia.com>
2016-06-09 21:36:54 +1200

    Code style.

--- src/Drush/ModulePermissions.php
+++ src/Drush/ModulePermissions.php
@@ -11,30 +11,32 @@ class ModulePermissions extends Check {
 
     $response->test(function ($check) {
       $context = $check->context;
-      //If Module Permissions enabled check only admin has permission to administer list of modules
+      // If Module Permissions enabled check only admin has permission to
+      // administer list of modules.
       if ($context->drush->moduleEnabled('module_permissions')) {
         $permissions = $context->drush->getRolesForPermission('administer module permissions');
         $roles = $context->drush->getAllRoles();
 
         foreach ($roles as $role) {
-          if(in_array($role, $permissions)) {
-            // For some reason when running against individual site it has an array key of 'role'
-            // but when running against whole ACSF it has the key of 'label'
-            if(isset($role['role'])) {
+          if (in_array($role, $permissions)) {
+            // For some reason when running against individual site it has an
+            // array key of 'role' but when running against the entire ACSF it
+            // has the key of 'label'.
+            if (isset($role['role'])) {
               $the_role = $role['role'];
             } else {
               $the_role = $role['label'];
             }
-            if($the_role !== 'administrator') {
+            if ($the_role !== 'administrator') {
               return AuditResponse::AUDIT_FAILURE;
             }
           }
         }
-        
+
         return AuditResponse::AUDIT_SUCCESS;
-      } else {
-        return AuditResponse::AUDIT_NA;
       }
+
+      return AuditResponse::AUDIT_NA;
     });
 
     return $response;
