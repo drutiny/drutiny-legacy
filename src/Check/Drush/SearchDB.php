a9bb11178284fdc692d445677270b6a445f332c6
Sean Hamlin <sean.hamlin@acquia.com>
2016-06-22 15:54:00 +1200

    Include the number of nodes in the DB index when it is being used.

--- src/Check/Drush/SearchDB.php
+++ src/Check/Drush/SearchDB.php
@@ -16,7 +16,22 @@ class SearchDB extends Check {
       throw new DoesNotApplyException();
     }
 
+    // Check if the database is used used to search.
     if ($this->context->drush->moduleEnabled('search_api_db')) {
+      // If the database is in use, find out how many nodes are in it.
+      $result = $this->context->drush->sqlq('SELECT COUNT(item_id) FROM search_api_db_default_node_index;');
+      $output = $result->getOutput();
+
+      // There are some differences in running the command on site factory then
+      // locally.
+      if (count($output) == 1) {
+        $nodes_in_search = (int) $output[0];
+      }
+      else {
+        $nodes_in_search = (int) $output[1];
+      }
+
+      $this->setToken('nodes_in_search', $nodes_in_search);
       return FALSE;
     }
 
