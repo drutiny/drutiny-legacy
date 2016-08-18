68be080158926b0d21bb18ef3f5d09102b0714a6
Sean Hamlin <sean.hamlin@acquia.com>
2016-08-19 10:05:22 +1200

    Secure pages has a bug that means we need to flip the logic around to force all sites HTTPS. See https://www.drupal.org/node/566632. This commit works around that issue.

--- src/Check/Drush/SecurepagesRedirectToHttps.php
+++ src/Check/Drush/SecurepagesRedirectToHttps.php
@@ -10,7 +10,7 @@ use SiteAudit\Executor\DoesNotApplyException;
  * @CheckInfo(
  *  title = "Securepages redirect to HTTPS",
  *  description = "The securepages module redirects the user to HTTPS given certain conditions. This check ensures that all traffic is sent to HTTPS.",
- *  remediation = "Set <code>securepages_enable</code> to 1, <code>securepages_switch</code> to 0, and <code>securepages_secure</code> to 0.",
+ *  remediation = "Set <code>securepages_enable</code> to <code>1</code>, <code>securepages_switch</code> to <code>0</code>, <code>securepages_secure</code> to <code>1</code>, <code>securepages_pages</code> to <code>*</code>.",
  *  success = "Securepages is redirectly all requests to HTTPS.",
  *  failure = "Securepages is not configured correctly to redirect all requests to HTTPS.",
  *  exception = "Could not determine securepages settings.",
@@ -21,14 +21,16 @@ class SecurepagesRedirectToHttps extends Check {
   {
     if ($this->context->drush->moduleEnabled('securepages')) {
       $securepages_enable = (bool) $this->context->drush->getVariable('securepages_enable', 0);
-      $securepages_switch = (bool) $this->context->drush->getVariable('securepages_switch', FALSE);
+      $securepages_switch = (bool) $this->context->drush->getVariable('securepages_switch', 0);
       $securepages_secure = (bool) $this->context->drush->getVariable('securepages_secure', 1);
+      $securepages_pages = $this->context->drush->getVariable('securepages_pages', '');
 
       // Securepages must:
       // * be enabled
       // * not switch back to HTTP
-      // * make all pages secure by default.
-      if ($securepages_enable && !$securepages_switch && !$securepages_secure) {
+      // * make secure only the listed pages
+      // * the listed pages is '*'.
+      if ($securepages_enable && !$securepages_switch && $securepages_secure && $securepages_pages === '*') {
         return TRUE;
       }
       else {
