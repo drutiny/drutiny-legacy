207b3b1402c50a3ac871cdf32f8cd37f8fe03985
Sean Hamlin <sean.hamlin@acquia.com>
2016-08-29 14:22:10 +1200

    Turns out that Drush lies when you ask for cron_safe_threshold, so get it from the DB.

--- src/Check/Drush/CronPoorMans.php
+++ src/Check/Drush/CronPoorMans.php
@@ -11,15 +11,18 @@ use SiteAudit\Annotation\CheckInfo;
  *  description = "Checks that poor mans cron is disabled and will never run with a web thread.",
  *  remediation = "Set the variable <code>cron_safe_threshold</code> to <code>0</code>.",
  *  success = "Poor mans cron is disabled.",
- *  failure = "Poor mans cron is still enabled, current set to run every <code>:cron_safe_threshold</code> seconds.",
+ *  failure = "Poor mans cron is still enabled, currently set to run every <code>:cron_safe_threshold</code> seconds.",
  *  exception = "Could not determine status of poor mans cron: :exception."
  * )
  */
 class CronPoorMans extends Check {
   public function check()
   {
-    // @see DRUPAL_CRON_DEFAULT_THRESHOLD which defaults to 10800.
-    $cron_safe_threshold = (int) $this->context->drush->getVariable('cron_safe_threshold', 10800);
+    // @see DRUPAL_CRON_DEFAULT_THRESHOLD which defaults to 10800. You also
+    // cannot get this variable via drush as it gets overridden, so do a manual
+    // database query instead.
+    // @see https://github.com/drush-ops/drush/pull/1662
+    $cron_safe_threshold = (int) $this->context->drush->getVariableFromDB('cron_safe_threshold', 10800);
     $this->setToken('cron_safe_threshold', $cron_safe_threshold);
     if ($cron_safe_threshold > 0) {
       return FALSE;
