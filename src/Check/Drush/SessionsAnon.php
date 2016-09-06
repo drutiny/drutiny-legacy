67539a855b604987f0115511379c9ad1ceec896e
Sean Hamlin <sean.hamlin@acquia.com>
2016-09-06 15:52:22 +1200

    Exclude openid from anon sessions, also better wording.

--- src/Check/Drush/SessionsAnon.php
+++ src/Check/Drush/SessionsAnon.php
@@ -11,14 +11,16 @@ use SiteAudit\Annotation\CheckInfo;
  *  description = "If you are generating sessions for anonymous users, you are causing a major performance impact to your site. Having anonymous sessions will break traditional page caching in Varnish and CDNs.",
  *  remediation = "Find out what modules are causing the sessions, and look to remove them.",
  *  success = "There are no anonymous sessions.",
- *  failure = "There are <code>:number_of_anon_sessions</code> anonymous sessions.",
+ *  failure = "There :prefix <code>:number_of_anon_sessions</code> anonymous session:plural.",
  *  exception = "Could not determine the amount of anonymous sessions.",
  * )
  */
 class SessionsAnon extends Check {
   public function check() {
 
-    $output = $this->context->drush->sqlQuery("SELECT COUNT(*) FROM {sessions} WHERE uid = 0;");
+    // Exclude openid sessions, as these are for people that are trying to login
+    // but perhaps have not made it the whole way yet.
+    $output = $this->context->drush->sqlQuery("SELECT COUNT(*) FROM {sessions} WHERE uid = 0 AND session NOT LIKE 'openid%';");
     if (empty($output)) {
       $number_of_anon_sessions = 0;
     }
@@ -29,6 +31,8 @@ class SessionsAnon extends Check {
       $number_of_anon_sessions = (int) $output[1];
     }
     $this->setToken('number_of_anon_sessions', $number_of_anon_sessions);
+    $this->setToken('plural', count($number_of_anon_sessions) > 1 ? 's' : '');
+    $this->setToken('prefix', count($number_of_anon_sessions) > 1 ? 'are' : 'is');
 
     return $number_of_anon_sessions === 0;
   }
