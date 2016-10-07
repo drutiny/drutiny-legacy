37b2eb6dbb7580bb6adf936bf6631673d04c7e73
Sean Hamlin <sean.hamlin@acquia.com>
2016-10-07 13:11:02 +1300

    Better webform checks.

--- src/Check/Drush/WebformUpload.php
+++ src/Check/Drush/WebformUpload.php
@@ -9,8 +9,9 @@ use SiteAudit\Executor\DoesNotApplyException;
 /**
  * @CheckInfo(
  *  title = "Webform upload",
- *  description = "Spammers are known to want to uplaod files to anonymous webforms.",
- *  remediation = "Restrict upload types, and sizes.",
+ *  description = "Spammers are known to want to uplaod files to webforms that allow anonymous user users access.",
+ *  remediation = "Restrict upload types, enforce a max upload size, use a random folder underneath <code>/webform/</code> to store the uploads.",
+ *  not_available = "Webform is not enabled.",
  *  success = "There are no files uploaded that look malicious.",
  *  failure = "There :prefix <code>:number_of_silly_uploads</code> malicious webform upload:plural.:files",
  *  exception = "Could not determine the amount of malicious uploads.",
