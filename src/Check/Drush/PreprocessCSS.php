cb7e2519b3b3cd347864c8c9744e01893e259560
Sean Hamlin <sean.hamlin@acquia.com>
2016-12-09 12:18:02 +1300

    More auto remediation in JS and CSS checks.

--- src/Check/Drush/PreprocessCSS.php
+++ src/Check/Drush/PreprocessCSS.php
@@ -10,14 +10,25 @@ use SiteAudit\Annotation\CheckInfo;
  *  title = "CSS aggregation",
  *  description = "With CSS optimization disabled, your website visitors are experiencing slower page performance and the server load is increased.",
  *  remediation = "Set the variable <code>preprocess_css</code> to be <code>1</code>.",
- *  success = "CSS aggregation is enabled.",
+ *  success = "CSS aggregation is enabled.:fixups",
  *  failure = "CSS aggregation is not enabled.",
  *  exception = "Could not determine CSS aggregation setting.",
+ *  supports_remediation = TRUE,
  * )
  */
 class PreprocessCss extends Check {
   public function check()
   {
-    return (bool) (int) $this->context->drush->getVariable('preprocess_css', 0);
+    $fixups = [];
+    $preprocess_css = (bool) (int) $this->context->drush->getVariable('preprocess_css', 0);
+
+    if (!$preprocess_css && $this->context->autoRemediate) {
+      $this->context->drush->variableSet('preprocess_css', 1);
+      $fixups[] = 'This was auto remediated.';
+      $preprocess_css = TRUE;
+    }
+
+    $this->setToken('fixups', ' ' . implode(', ', $fixups) . '.');
+    return $preprocess_css;
   }
 }
--- src/Check/Drush/PreprocessJS.php
+++ src/Check/Drush/PreprocessJS.php
@@ -7,17 +7,28 @@ use SiteAudit\Annotation\CheckInfo;
 
 /**
  * @CheckInfo(
- * title = "JS aggregation",
- * description = "With JS optimization disabled, your website visitors are experiencing slower page performance and the server load is increased.",
- * remediation = "Set the variable <code>preprocess_js</code> to be <code>1</code>.",
- * success = "JS aggregation is enabled.",
- * failure = "JS aggregation is not enabled.",
- * exception = "Could not determine JS aggregation setting.",
+ *  title = "JS aggregation",
+ *  description = "With JS optimization disabled, your website visitors are experiencing slower page performance and the server load is increased.",
+ *  remediation = "Set the variable <code>preprocess_js</code> to be <code>1</code>.",
+ *  success = "JS aggregation is enabled.:fixups",
+ *  failure = "JS aggregation is not enabled.",
+ *  exception = "Could not determine JS aggregation setting.",
+ *  supports_remediation = TRUE,
  * )
  */
 class PreprocessJS extends Check {
   public function check()
   {
-    return (bool) (int) $this->context->drush->getVariable('preprocess_js', 0);
+    $fixups = [];
+    $preprocess_js = (bool) (int) $this->context->drush->getVariable('preprocess_js', 0);
+
+    if (!$preprocess_js && $this->context->autoRemediate) {
+      $this->context->drush->variableSet('preprocess_js', 1);
+      $fixups[] = 'This was auto remediated.';
+      $preprocess_js = TRUE;
+    }
+
+    $this->setToken('fixups', ' ' . implode(', ', $fixups) . '.');
+    return $preprocess_js;
   }
 }
