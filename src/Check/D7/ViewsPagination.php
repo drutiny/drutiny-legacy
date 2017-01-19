24b050152298417efe0f3253ec9c2237ad59c989
Steven Worley <sj.worley88@gmail.com>
2017-01-20 08:51:55 +1000

    Add views pagination check

---
+++ src/Check/D7/ViewsPagination.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @file
+ * Contains SiteAudit\Check\D7\ViewsPagination
+ */
+
+namespace SiteAudit\Check\D7;
+
+use SiteAudit\Check\Check;
+use SiteAudit\Annotation\CheckInfo;
+
+
+/**
+ * @CheckInfo(
+ *   title = "Views Pagination",
+ *   description = "Ensure views pagination is not over a threshold",
+ *   remediation = "Change the following views pagination settings to below :threshold: <ul><li>:error</li></ul>",
+ *   success = "Found <code>:total</code> view:plural that are correctly paginating results.",
+ *   failure = "Found <code>:error_count</code> view:plural that pagination exceeds threshold.",
+ *   exception = "Error finding views",
+ *   not_available = "No views found.",
+ * )
+ */
+class ViewsPagination extends Check {
+  public function check() {
+    // We don't need to perform these checks if the entity reference module is
+    // not enabled.
+    if (!$this->context->drush->moduleEnabled('views')) {
+      return AuditResponse::AUDIT_NA;
+    }
+
+    $valid = 0;
+    $errors = [];
+
+    // View settings are set per display so we need to query the views display table.
+    $views = $this->context->drush->sqlQuery("SELECT vd.id, vd.display_title, vd.display_options, vv.name, vv.human_name FROM {views_data} JOIN {views_view} ON vv.vid = vd.vid");
+
+    foreach ($views as $view) {
+      list($display_id, $display_name, $display_options, $view_machine_name, $view_name) = explode("\t", $view);
+      $display_options = unserialize($display_options);
+
+      if ($display_options['pager']['options']['items_per_page'] > $this->getOption('threshold', 30)) {
+        $errors[] = "$view_name [$display_name] is displaying <code>{$display_options['pager']['options']['items_per_page']}</code>";
+        continue;
+      }
+
+      $valid++;
+    }
+
+    $this->setToken('total', $valid);
+    $this->setToken('plural', $valid > 1 ? 's' : '');
+    $this->setToken('error', implode('</li><li>',  $this->errors));
+    $this->setToken('error_count', count($this->errors));
+  }
+}
