fb79101928fc801aee9f890a28444c4cc573b4dc
Sean Hamlin <sean.hamlin@acquia.com>
2017-03-06 23:34:49 +1300

    Add auto remediation to error_level.

--- src/Check/D7/ErrorLevel.php
+++ src/Check/D7/ErrorLevel.php
@@ -10,12 +10,17 @@ use Drutiny\Annotation\CheckInfo;
  *  title = "Error level",
  *  description = "When PHP encounters an error, it can generate an error log and display a report on the screen. While these error messages can be helpful in debugging your site, they can be a security risk on a live site as they may reveal information about your server that can be used to compromise it.",
  *  remediation = "Set the variable <code>error_level</code> to be <code>0</code>.",
- *  success = "Errors are not shown on the screen.",
+ *  success = "Errors are not shown on the screen.:fixups",
  *  failure = "Errors are shown on the screen, currently set to log <code>:error_level</code>.",
  *  exception = "Could not determine error level setting.",
+ *  supports_remediation = TRUE,
  * )
  */
 class ErrorLevel extends Check {
+
+  /**
+   * @inheritDoc
+   */
   public function check()
   {
     $error_level = (int) $this->context->drush->getVariable('error_level', 2);
@@ -27,5 +32,14 @@ class ErrorLevel extends Check {
     ];
     $this->setToken('error_level', $human_names[$error_level]);
     return $error_level === 0;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function remediate()
+  {
+    $res = $this->context->drush->setVariable('error_level', 0);
+    return $res->isSuccessful();
   }
 }
