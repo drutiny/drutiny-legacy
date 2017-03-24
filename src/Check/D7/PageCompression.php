38f42091b7ae147ac293a82d25de6d100439f433
Sean Hamlin <sean.hamlin@acquia.com>
2017-03-24 20:56:34 +1300

    Add rememdiation to page_compression.

--- src/Check/D7/PageCompression.php
+++ src/Check/D7/PageCompression.php
@@ -11,18 +11,27 @@ use Drutiny\Check\Check;
  *  remediation = "Set the variable <code>page_compression</code> to <code>0</code>.",
  *  success = "Compress cached pages is disabled.",
  *  failure = "Compress cached pages (page_compression) is enabled.",
- *  exception = "Could not determine status of page_compression: :exception."
+ *  exception = "Could not determine status of page_compression: :exception.",
+ *  supports_remediation = TRUE,
  * )
  */
 class PageCompression extends Check {
 
   /**
-   *
+   * @inheritDoc
    */
   public function check() {
     $page_compression = (bool) $this->context->drush->getVariable('page_compression', TRUE);
     $this->setToken('page_compression', $page_compression);
     return !$page_compression;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function remediate() {
+    $res = $this->context->drush->setVariable('page_compression', 0);
+    return $res->isSuccessful();
   }
 
 }
