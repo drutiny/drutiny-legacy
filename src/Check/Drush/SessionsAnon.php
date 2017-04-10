415dbd0cd70f1ffe8e956624dc7b88cc03c41231
Sean Hamlin <sean.hamlin@acquia.com>
2017-04-10 16:35:59 +1200

    Fix undefined offset issue.

--- src/Check/Drush/SessionsAnon.php
+++ src/Check/Drush/SessionsAnon.php
@@ -24,12 +24,16 @@ class SessionsAnon extends Check {
     // Exclude openid sessions, as these are for people that are trying to login
     // but perhaps have not made it the whole way yet.
     $output = $this->context->drush->sqlQuery("SELECT session FROM {sessions} WHERE uid = 0 AND session NOT LIKE 'openid%' AND session NOT LIKE '%Access denied%';");
+    $output = array_filter($output);
     $number_of_anon_sessions = count($output);
 
     $this->setToken('number_of_anon_sessions', $number_of_anon_sessions);
     $this->setToken('plural', $number_of_anon_sessions > 1 ? 's' : '');
     $this->setToken('prefix', $number_of_anon_sessions > 1 ? 'are' : 'is');
-    $this->setToken('sample', $output[1]);
+
+    if ($number_of_anon_sessions > 0) {
+      $this->setToken('sample', $output[0]);
+    }
 
     return $number_of_anon_sessions === 0;
   }
