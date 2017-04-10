90ac32bbf08388c98892356e8ea69a760c1a7a88
Sean Hamlin <sean.hamlin@acquia.com>
2017-04-10 23:23:19 +1200

    More robust session output sample.

--- src/Check/Drush/SessionsAnon.php
+++ src/Check/Drush/SessionsAnon.php
@@ -27,13 +27,22 @@ class SessionsAnon extends Check {
     $output = array_filter($output);
     $number_of_anon_sessions = count($output);
 
-    $this->setToken('number_of_anon_sessions', $number_of_anon_sessions);
+    $this->setToken('number_of_anon_sessions', number_format($number_of_anon_sessions));
     $this->setToken('plural', $number_of_anon_sessions > 1 ? 's' : '');
     $this->setToken('prefix', $number_of_anon_sessions > 1 ? 'are' : 'is');
 
-    if ($number_of_anon_sessions > 0) {
-      $this->setToken('sample', $output[0]);
+    // Work out a sample session, excluding the column name.
+    $sample = '';
+    $output = array_slice($output, 0, 10);
+    foreach ($output as $index => $row) {
+      if ($row === 'session') {
+        continue;
+      }
+      $sample = $row;
+      break;
     }
+
+    $this->setToken('sample', $sample);
 
     return $number_of_anon_sessions === 0;
   }
