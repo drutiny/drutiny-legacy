16d893fd13f768e5be83c6996cb3b7e6333518d1
Josh Waihi <josh.waihi@acquia.com>
2017-04-10 16:19:32 +1200

    Made output of AnonSession check more valuable

--- src/Check/Drush/SessionsAnon.php
+++ src/Check/Drush/SessionsAnon.php
@@ -10,7 +10,7 @@ use Drutiny\Check\Check;
  *  description = "If you are generating sessions for anonymous users, you are causing a major performance impact to your site. Having anonymous sessions will break traditional page caching in Varnish and CDNs.",
  *  remediation = "Find out what modules are causing the sessions, and look to remove them.",
  *  success = "There are no anonymous sessions.",
- *  failure = "There :prefix <code>:number_of_anon_sessions</code> anonymous session:plural.",
+ *  failure = "There :prefix <code>:number_of_anon_sessions</code> anonymous session:plural. Sample session content: <p><code>:sample</code></p>",
  *  exception = "Could not determine the amount of anonymous sessions.",
  * )
  */
@@ -23,19 +23,13 @@ class SessionsAnon extends Check {
 
     // Exclude openid sessions, as these are for people that are trying to login
     // but perhaps have not made it the whole way yet.
-    $output = $this->context->drush->sqlQuery("SELECT COUNT(*) FROM {sessions} WHERE uid = 0 AND session NOT LIKE 'openid%' AND session NOT LIKE '%Access denied%';");
-    if (empty($output)) {
-      $number_of_anon_sessions = 0;
-    }
-    elseif (count($output) == 1) {
-      $number_of_anon_sessions = (int) $output[0];
-    }
-    else {
-      $number_of_anon_sessions = (int) $output[1];
-    }
+    $output = $this->context->drush->sqlQuery("SELECT session FROM {sessions} WHERE uid = 0 AND session NOT LIKE 'openid%' AND session NOT LIKE '%Access denied%';");
+    $number_of_anon_sessions = count($output);
+
     $this->setToken('number_of_anon_sessions', $number_of_anon_sessions);
     $this->setToken('plural', $number_of_anon_sessions > 1 ? 's' : '');
     $this->setToken('prefix', $number_of_anon_sessions > 1 ? 'are' : 'is');
+    $this->setToken('sample', $output[1]);
 
     return $number_of_anon_sessions === 0;
   }
