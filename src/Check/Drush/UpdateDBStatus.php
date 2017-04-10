101ea952d53c19eb9de2bf602d012e0dc7e4b24d
Sean Hamlin <sean.hamlin@acquia.com>
2017-04-10 23:23:43 +1200

    Prevetn false positives on updatedb with missing modules.

--- src/Check/Drush/UpdateDBStatus.php
+++ src/Check/Drush/UpdateDBStatus.php
@@ -17,16 +17,31 @@ use Drutiny\Check\Check;
 class UpdateDBStatus extends Check {
 
   /**
-   *
+   * @inheritDoc
    */
   public function check() {
     $context = $this->context;
     $output = $context->drush->updatedbStatus()->getOutput();
+
+    // After Drupal 7.50 there are a lot more errors that can effect this
+    // output. Strip them out.
+    foreach ($output as $index => $row) {
+      if (preg_match('/^The following module is missing from the file system:/', $row)) {
+        unset($output[$index]);
+      }
+      if (preg_match('/https:\/\/www\.drupal\.org\/node\/2487215/', $row)) {
+        unset($output[$index]);
+      }
+    }
+
+    $output = array_filter($output);
+
     // Sometimes "No database updates required" is in Stderr, and thus is
     // empty.
     if (empty($output)) {
       return TRUE;
     }
+
     if (count($output) === 1) {
       $output = reset($output);
       if (strpos($output, 'No database updates required') === 0 || empty($output)) {
