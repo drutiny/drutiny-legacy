17ea0d53d509ecd5eb3d78fec5efc4b1a37c4b6e
Sean Hamlin <sean.hamlin@acquia.com>
2017-04-11 22:09:55 +1200

    When the database size exceeds the max_size, find out the top 3 tables. Fixes #76.

--- src/Check/Drush/DatabaseSize.php
+++ src/Check/Drush/DatabaseSize.php
@@ -12,7 +12,7 @@ use Drutiny\AuditResponse\AuditResponse;
  *  remediation = "Find out what tables are the largest and see what can be done to make them smaller.",
  *  success = "The size of the database <code>:db</code> is good, currently <code>:size</code> MB.",
  *  warning = "The size of the database <code>:db</code> could be better, currently <code>:size</code> MB.",
- *  failure = "The size of the database <code>:db</code> is too large, currently <code>:size</code> MB.",
+ *  failure = "The size of the database <code>:db</code> is too large, currently <code>:size</code> MB. The largest tables are - <ol><li><code>:tables</code></li></ol>",
  *  exception = "Could not determine database size.",
  * )
  */
@@ -35,7 +35,27 @@ class DatabaseSize extends Check {
     $this->setToken('db', $db);
     $this->setToken('size', number_format($size, 1));
 
+    // Find the top tables by size when the check is above the maximum size, to
+    // better inform the developers on how to fix.
     if ($size >= $max_size) {
+      $rows = $this->context->drush->sqlQuery("SELECT table_name AS 'Table', ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) 'Table Size in MB' FROM information_schema.tables WHERE table_schema='{$db}' GROUP BY table_name ORDER BY (data_length + index_length) DESC LIMIT 3;");
+      $rows = array_filter($rows);
+      $rows = array_map('trim', $rows);
+
+      // Split by tab.
+      $tables = [];
+      foreach ($rows as $row) {
+        if (preg_match('/^Table/', $row)) {
+          continue;
+        }
+        $parts = explode("\t", $row);
+        $table = $parts[0];
+        $size = number_format((float) $parts[1], 1);
+        $tables[] = "{$table} [{$size} MB]";
+      }
+
+      $this->setToken('tables', implode('</code></li><li><code>', $tables));
+
       return AuditResponse::AUDIT_FAILURE;
     }
     if ($size >= $warning_size) {
