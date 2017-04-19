91ce1f2bc6855912cb9e86369a96265bbbb0a359
Sean Hamlin <sean.hamlin@acquia.com>
2017-04-20 11:08:55 +1200

    Keep the D7 page cache check more inline with the D8 version.

--- src/Check/D7/PageCacheMaximumAge.php
+++ src/Check/D7/PageCacheMaximumAge.php
@@ -7,7 +7,7 @@ use Drutiny\Check\Check;
 /**
  * @Drutiny\Annotation\CheckInfo(
  *  title = "Page cache",
- *  description = "With a page cache max age set to less than 5 minutes, the server has to frequently regenerate pages which can decrease your site's performance.",
+ *  description = "Ensure you page cache expiry is set to an optimal level for best performance.",
  *  remediation = "Set the variable <code>page_cache_maximum_age</code> to be greater than <code>300</code>.",
  *  success = "Page cache max-age is set above <code>:cache</code> seconds. Currently set to <code>:page_cache_maximum_age</code>.",
  *  failure = "Page cache max-age is not set above <code>:cache</code> seconds. Currently set to <code>:page_cache_maximum_age</code>.",
@@ -17,12 +17,23 @@ use Drutiny\Check\Check;
 class PageCacheMaximumAge extends Check {
 
   /**
-   *
+   * @inheritDoc
    */
   public function check() {
+    $cache = (int) $this->getOption('cache', 300);
     $page_cache_maximum_age = (int) $this->context->drush->getVariable('page_cache_maximum_age', 0);
+    $this->setToken('cache', $cache);
     $this->setToken('page_cache_maximum_age', $page_cache_maximum_age);
-    return ($page_cache_maximum_age >= $this->getOption('cache', 300));
+    return $page_cache_maximum_age >= $cache;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function remediate() {
+    $cache = (int) $this->getOption('cache', 300);
+    $res = $this->context->drush->setVariable('page_cache_maximum_age', $cache);
+    return $res->isSuccessful();
   }
 
 }
