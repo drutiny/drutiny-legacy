b441f749367ea8be136f83d06cd8024a9103611f
Sean Hamlin <sean.hamlin@acquia.com>
2017-04-20 11:07:57 +1200

    Better descriptions for D8 pagecache.

--- src/Check/D8/PageCache.php
+++ src/Check/D8/PageCache.php
@@ -8,9 +8,9 @@ use Drutiny\Check\Check;
  * @Drutiny\Annotation\CheckInfo(
  *  title = "Page Cache",
  *  description = "Ensure you page cache expiry is set to an optimal level for best performance.",
- *  remediation = "Set the configuration object <code>system.performance</code> key <code>cache.page.max_age</code> to :page_cache.",
- *  success = "Page cache is configured correctly.",
- *  failure = "Page cache settings (cache.page.max_age) are not configured for optimal performance. Setting is :page_cache_lifetime seconds.",
+ *  remediation = "Set the configuration object <code>system.performance</code> key <code>cache.page.max_age</code> to <code>:page_cache</code>.",
+ *  success = "Page cache is set above <code>:page_cache</code> seconds. Currently set to <code>:current</code>.",
+ *  failure = "Page cache is not set above <code>:page_cache</code> seconds. Currently set to <code>:current</code>.",
  *  exception = "Could not determine page cache settings.",
  *  supports_remediation = TRUE,
  * )
@@ -21,18 +21,19 @@ class PageCache extends Check {
    * @inheritDoc
    */
   public function check() {
-    $lifetime = $this->getOption('page_cache', 3600);
-    $setting = $this->context->drush->getConfig('system.performance', 'cache.page.max_age');
-    $this->setToken('page_cache_lifetime', $setting);
-    return $setting > $lifetime;
+    $page_cache = (int) $this->getOption('page_cache', 3600);
+    $current = (int) $this->context->drush->getConfig('system.performance', 'cache.page.max_age');
+    $this->setToken('page_cache', $page_cache);
+    $this->setToken('current', $current);
+    return $current >= $page_cache;
   }
 
   /**
    * @inheritDoc
    */
   public function remediate() {
-    $lifetime = $this->getOption('page_cache', 3600);
-    $res = $this->context->drush->configSet('system.performance', 'cache.page.max_age', $lifetime);
+    $page_cache = (int) $this->getOption('page_cache', 3600);
+    $res = $this->context->drush->configSet('system.performance', 'cache.page.max_age', $page_cache);
     return $res->isSuccessful();
   }
 
